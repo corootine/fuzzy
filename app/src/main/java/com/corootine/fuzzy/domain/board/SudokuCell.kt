e3dc111fa493c311e4feb0c66e549528750c41a7
corootine <ahmed.arnaut@hotmail.com>
2020-09-06 22:12:13 +0200

    Improved cell builder and provided a way to access the cell values.

--- app/src/main/java/com/corootine/fuzzy/domain/board/SudokuCell.kt
+++ app/src/main/java/com/corootine/fuzzy/domain/board/SudokuCell.kt
@@ -1,55 +1,69 @@
 package com.corootine.fuzzy.domain.board
 
+import com.corootine.fuzzy.domain.board.SudokuCell.Builder
 import timber.log.Timber
 
-private const val SIZE = 3
-
 /**
- * Immutable representation of a sudoku cell of size [SIZE]x[SIZE].
+ * Immutable representation of a sudoku cell. Use [Builder] to construct an instance of this class.
  */
-// TODO: 03/09/20 Convert this array to an immutable collection and provide accessors
-class SudokuCell(cells: Array<Array<Int>>) {
+class SudokuCell private constructor(private val cell: Array<IntArray>, private val size: Int) {
 
-    class Builder {
+    /**
+     * Gets a value associated to the given row and column. Index of row and column must be between 0 and [size].
+     * If the index is not within these constraints, this method will throw an [IllegalArgumentException].
+     *
+     * @throws IllegalArgumentException in case row or column index is out of range
+     */
+    operator fun get(row: Int, column: Int): Int {
+        require(row in 0 until size)
+        require(column in 0 until size)
 
-        private val cells = arrayOf(
-            arrayOf(0, 0, 0),
-            arrayOf(0, 0, 0),
-            arrayOf(0, 0, 0)
-        )
+        return cell[row][column]
+    }
+
+    class Builder(private val size: Int) {
+
+        private var cell: Array<IntArray> = Array(size) { IntArray(3) { 0 } }
 
         /**
-         * Attempts to set [value] for the given [row] and [column]. Specified [row] and [column] must be between 0 and [SIZE].
-         * In case this precondition is not met, this method will fail with an [IllegalArgumentException].
-         * If the provided value already appears somewhere else in this cell, it will not be set again and this method will return false.
-         * If the value appears for the first time, it will be set for the given [row] and [column] and this method will return true.
+         * Attempts to set [value] for the given [row] and [column]. Specified [row] and [column]
+         * must be between 0 and [size]. In case this precondition is not met, this method
+         * will fail with an [IllegalArgumentException].
+         *
+         * @param row row inside the cell, must be between 0 and [size]
+         * @param column column inside the cell, must be between 0 and [size]
+         * @throws IllegalArgumentException in case of invalid row or column size
          */
-        fun set(row: Int, column: Int, value: Int): Boolean {
-            require(row in 0 until SIZE)
-            require(column in 0 until SIZE)
+        fun set(row: Int, column: Int, value: Int) {
+            require(row in 0 until size)
+            require(column in 0 until size)
 
-            return if (valueExists(value)) {
-                Timber.d("Cell already contains the value '$value'.")
-                false
-            } else {
-                Timber.d("Setting value '$value' to [$row][$column].")
-                cells[row][column] = value
-                true
-            }
+            Timber.d("Setting value '$value' to [$row][$column].")
+            cell[row][column] = value
         }
 
-        private fun valueExists(value: Int): Boolean {
-            for (row in cells) {
-                for (currentValue in row) {
-                    if (currentValue == value) {
-                        return true
+        /**
+         * Builds the [SudokuCell]. This method will check that all values inside this cell are
+         * unique and there are no duplicates. If there are duplicates, this method will fail with
+         * an [IllegalStateException]
+         *
+         * @throws IllegalStateException in case of duplicate values.
+         */
+        fun build(): SudokuCell {
+            val values = HashSet<Int>()
+            for (row in cell) {
+                for (value in row) {
+                    if (values.contains(value)) {
+                        val exception = IllegalStateException("Cannot build due to duplicate value '$value'.")
+                        Timber.e(exception)
+                        throw exception
+                    } else {
+                        values.add(value)
                     }
                 }
             }
 
-            return false
+            return SudokuCell(cell, size)
         }
-
-        fun build() = SudokuCell(cells)
     }
 }
--- app/src/test/java/com/corootine/fuzzy/domain/board/SudokuCellTest.kt
+++ app/src/test/java/com/corootine/fuzzy/domain/board/SudokuCellTest.kt
@@ -2,121 +2,77 @@ package com.corootine.fuzzy.domain.board
 
 import org.junit.Assert.assertEquals
 import org.junit.Test
-import org.junit.experimental.runners.Enclosed
-import org.junit.runner.RunWith
-import org.junit.runners.Parameterized.Parameters
 
-@RunWith(Enclosed::class)
 class SudokuCellTest {
+    
+    @Test(expected = IllegalArgumentException::class)
+    fun `should not allow negative row size`() {
+        // given
+        val builder = SudokuCell.Builder(3)
 
-    class NonParameterized {
+        // when
+        builder.set(-1, 2, 5)
+    }
 
-        @Test(expected = IllegalArgumentException::class)
-        fun `should not allow negative row size`() {
-            // given
-            val builder = SudokuCell.Builder()
+    @Test(expected = IllegalArgumentException::class)
+    fun `should not allow negative column size`() {
+        // given
+        val builder = SudokuCell.Builder(3)
 
-            // when
-            builder.set(-1, 2, 5)
+        // when
+        builder.set(2, -1, 5)
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun `should not allow row size greater than 2`() {
+        // given
+        val builder = SudokuCell.Builder(3)
+
+        // when
+        builder.set(3, 2, 5)
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun `should not allow column size greater than 2`() {
+        // given
+        val builder = SudokuCell.Builder(3)
+
+        // when
+        builder.set(2, 3, 5)
+    }
+
+    @Test
+    fun `should create a sudoku cell`() {
+        // given
+        val builder = SudokuCell.Builder(3)
+        val cell = arrayOf(
+            arrayOf(1, 2, 4),
+            arrayOf(5, 8, 9),
+            arrayOf(6, 7, 3)
+        )
+
+        // when
+        for (row in cell.indices) {
+            for (column in cell[row].indices) {
+                builder.set(row, column, cell[row][column])
+            }
         }
+        val sudokuCell = builder.build()
 
-        @Test(expected = IllegalArgumentException::class)
-        fun `should not allow negative column size`() {
-            // given
-            val builder = SudokuCell.Builder()
-
-            // when
-            builder.set(2, -1, 5)
-        }
-
-        @Test(expected = IllegalArgumentException::class)
-        fun `should not allow row size greater than 2`() {
-            // given
-            val builder = SudokuCell.Builder()
-
-            // when
-            builder.set(3, 2, 5)
-        }
-
-        @Test(expected = IllegalArgumentException::class)
-        fun `should not allow column size greater than 2`() {
-            // given
-            val builder = SudokuCell.Builder()
-
-            // when
-            builder.set(2, 3, 5)
+        // then
+        for (row in cell.indices) {
+            for (column in cell[row].indices) {
+                assertEquals(cell[row][column], sudokuCell[row, column])
+            }
         }
     }
 
-    @RunWith(org.junit.runners.Parameterized::class)
-    class Parameterized(private val input: Array<Array<Int>>, private val expected: Boolean) {
+    @Test(expected = IllegalStateException::class)
+    fun `should not allow duplicates`() {
+        // given
+        val builder = SudokuCell.Builder(3)
 
-        companion object {
-
-            @JvmStatic
-            @Parameters
-            fun data() = arrayOf(
-                arrayOf(
-                    arrayOf(
-                        arrayOf(1, 2, 4),
-                        arrayOf(5, 8, 9),
-                        arrayOf(6, 7, 3)
-                    ),
-                    true
-                ),
-                arrayOf(
-                    arrayOf(
-                        arrayOf(1, 2, 4),
-                        arrayOf(5, 4, 9),
-                        arrayOf(6, 7, 3)
-                    ),
-                    false
-                ),
-                arrayOf(
-                    arrayOf(
-                        arrayOf(9, 2, 4),
-                        arrayOf(5, 8, 1),
-                        arrayOf(6, 7, 9)
-                    ),
-                    false
-                ),
-                arrayOf(
-                    arrayOf(
-                        arrayOf(1, 1, 4),
-                        arrayOf(5, 8, 9),
-                        arrayOf(6, 7, 3)
-                    ),
-                    false
-                ),
-                arrayOf(
-                    arrayOf(
-                        arrayOf(1, 2, 9),
-                        arrayOf(8, 5, 4),
-                        arrayOf(7, 6, 3)
-                    ),
-                    true
-                ),
-            )
-        }
-
-        @Test
-        fun `should not allow duplicate numbers`() {
-            // given
-            val builder = SudokuCell.Builder()
-
-            // when
-            for (row in input.indices) {
-                for (column in input[row].indices) {
-                    println("Setting value ${input[row][column]} to [$row][$column]")
-                    val result = builder.set(row, column, input[row][column])
-                    if (!result) {
-                        assertEquals(expected, false)
-                        return
-                    }
-                }
-            }
-
-            assertEquals(expected, true)
-        }
+        // when
+        builder.build()
     }
 }
