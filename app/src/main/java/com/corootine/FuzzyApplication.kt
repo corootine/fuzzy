303536147e63bd59a59411b92d9d8300c1600931
corootine <ahmed.arnaut@hotmail.com>
2020-09-09 00:50:11 +0200

    Implemented a sudoku board with all the constraints validation.

--- app/src/main/java/com/corootine/FuzzyApplication.kt
+++ app/src/main/java/com/corootine/FuzzyApplication.kt
@@ -1,12 +1,10 @@
 package com.corootine
 
 import android.app.Application
-import timber.log.Timber
 
 class FuzzyApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        Timber.plant(Timber.DebugTree())
     }
 }
--- app/src/main/java/com/corootine/fuzzy/domain/board/SudokuBoard.kt
+++
@@ -1,6 +0,0 @@
-package com.corootine.fuzzy.domain.board
-
-import androidx.annotation.Size
-
-class SudokuBoard(@Size(9) val rows: IntArray, @Size(9) val columns: IntArray) {
-}
